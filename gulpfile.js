de8594a9e71e783bf4f49c4ea7473cc7059eb4a1
★Rogelio Oseguera de la Cruz★¹·² <rogeliooseguera16@gmail.com>
2020-08-20 19:40:36 -0500

    Proyecto finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,55 @@
+//Configuracion de Gulp para compilar archivos SASS
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
+sass.compiler = require('node-sass')
+
 function css(){
     return gulp
-    .src('scss/app.scss')
+    .src('./scss/*.scss')
+    .pipe(sass())
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 2 versions'],
         cascade : false
     }))
-    .pipe(sass({
-        outputStyle: 'expanded' //otras opciones: nested, compact, compressed
-    }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    
+    // .pipe(sass({
+    //     outputStyle: 'expanded' //otras opciones: nested, compact, compressed
+    // }))
+    // .pipe(gulp.dest('css'));
 }
 
 function watchFiles(){
-    gulp.watch('scss/*.scss', css)
+    gulp.watch('./scss/*.scss', css);
+    gulp.watch('index.html');
 }
 
 //Registrar funciones como tarea (Esto sirve para correr las funciones)
 gulp.task('css', css);
 gulp.task('watch', gulp.parallel(watchFiles));
+
+
+
+
+// //Configuracion de Gulp para compilar archivos SASS
+// const gulp = require('gulp'),
+//       sass = require('gulp-sass'),
+//       autoprefixer = require('gulp-autoprefixer');
+
+// gulp.task('sass', () => {
+//     gulp.src('scss/app.scss')
+//         .pipe(autoprefixer({
+//             browserlist: ['last 2 versions'],
+//             cascade : false
+//         }))
+//         .pipe(sass({
+//             includePaths: ['scss']
+//         }))
+//         .pipe(gulp.dest('css'));
+// });
+
+// gulp.task('watch', ['sass'], () => {
+//     gulp.watch(['scss/*.css'], ['sass']);
+// });
